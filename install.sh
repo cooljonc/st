87d5bf8b2647220ed0ebf061fb1d06c00483d698
cooljonc <91816521+cooljonc@users.noreply.github.com>
2023-12-13 11:05:21 +0100

    commit

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ if [ "$(id -u)" != '0' ]; then
 	exit 1
 fi
 
-if cd "$(dirname -- "$(readlink -f -- "$0")")"; then
+if cd "$(dirname -- "$0")"; then
 	make clean install
 	make clean
 	rm -f config.h
